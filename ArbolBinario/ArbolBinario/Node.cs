a7665ff57aa61a86c7ccca59edbedc94f57795ed
tomas <tmgarcia@miuandes.cl>
2018-04-24 16:37:38 -0300

    Avance

---
+++ ArbolBinario/ArbolBinario/Node.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArbolBinario
+{
+    class Node
+    {
+        public Node Padre;
+        public Node Izq;
+        public Node Der;
+        public Node fin;
+        public Node()
+        {
+        }
+
+        public void inicio()
+        {
+            Padre = null;
+        }
+
+        public void recorrido()
+        {
+
+        }
+
+
+    }
+}
